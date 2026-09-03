82f0669f004ad1904db8b896de24915fd1a9bcd0
arshadakl <66788460+arshadakl@users.noreply.github.com>
2026-09-03 15:39:49 +0530

    fix(templates): update TemplateRenderer for async component loading
    - Use defineAsyncComponent for template imports
    - Add template-editor class to wrapper
    - Update main.css with 12 font imports and CSS variables
    - Add font-display, font-script, font-serif-cinzel theme tokens
    - Add new fontsource packages to package.json

--- apps/web/app/components/templates/TemplateRenderer.vue
+++ apps/web/app/components/templates/TemplateRenderer.vue
@@ -1,12 +1,33 @@
 <script setup lang="ts">
-import type { PublicInvitation } from '@vowly/types'
-import FloralTemplate from '~/components/templates/floral/FloralTemplate.vue'
-import BurgundyTemplate from '~/components/templates/burgundy/BurgundyTemplate.vue'
+import { defineAsyncComponent, type Component } from 'vue'
+import type { PublicInvitation, TemplateId, TemplateTextStyle } from '@vowly/types'
+import { templateEditorKey } from '~/composables/useTemplateEditor'
+import { templateComponents } from '~/utils/templates'
 
-defineProps<{ invitation: PublicInvitation }>()
+const props = withDefaults(defineProps<{ invitation: PublicInvitation; editable?: boolean }>(), {
+  editable: false,
+})
+const emit = defineEmits<{
+  field: [field: string, value: string]
+  style: [field: string, style: TemplateTextStyle]
+}>()
+
+const invitationRef = toRef(props, 'invitation')
+const editableRef = toRef(props, 'editable')
+const asyncComponents = {} as Record<TemplateId, Component>
+for (const id of Object.keys(templateComponents) as TemplateId[]) {
+  asyncComponents[id] = defineAsyncComponent(templateComponents[id])
+}
+const component = computed(() => asyncComponents[props.invitation.template])
+
+provide(templateEditorKey, {
+  invitation: invitationRef,
+  editable: editableRef,
+  updateField: (field, value) => emit('field', field, value),
+  updateStyle: (field, style) => emit('style', field, style),
+})
 </script>
 
 <template>
-  <BurgundyTemplate v-if="invitation.template === 'burgundy'" :invitation="invitation" />
-  <FloralTemplate v-else :invitation="invitation" />
+  <component :is="component" :invitation="invitation" />
 </template>
